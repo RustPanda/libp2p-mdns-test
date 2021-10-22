e5d617b40cb197da589caeccd36f9f5b06d82df1
Matvei <goubevmt@gmail.com>
2021-10-22 19:10:55 +0300

    Обноружен -Ю Обнаружет

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> Result<()> {
     let peer_id = keypair.to_peer_id();
 
     let transport = QuicConfig::<Crypto>::new(keypair)
-        .listen_on("/ip4/127.0.0.2/udp/0/quic".parse()?)
+        .listen_on("/ip4/192.168.25.64/udp/0/quic".parse()?)
         .await?
         .boxed();
 
@@ -36,14 +36,14 @@ async fn main() -> Result<()> {
         }))
         .build();
 
-    swarm.listen_on("/ip4/127.0.0.2/udp/0/quic".parse()?)?;
+    swarm.listen_on("/ip4/192.168.25.64/udp/0/quic".parse()?)?;
 
     loop {
         if let SwarmEvent::Behaviour(event) = swarm.select_next_some().await {
             match event {
                 MdnsEvent::Discovered(peers) => peers
                     .into_iter()
-                    .for_each(|(peer, addr)| info!("Обноружен: heer_id: {} addr: {}", peer, addr)),
+                    .for_each(|(peer, addr)| info!("Обнaружен: heer_id: {} addr: {}", peer, addr)),
                 MdnsEvent::Expired(peers) => peers
                     .into_iter()
                     .for_each(|(peer, addr)| info!("Потерян: heer_id: {} addr: {}", peer, addr)),
